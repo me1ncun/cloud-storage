8b970f1e7b87fcf208aaac06c685de31b9d0edd8
Denis <denis09112004@gmail.com>
2024-04-22 20:47:32 +0300

    add database

---
+++ cloudfilestorage/Database/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace cloudfilestorage.Database;
+
+public class AppDbContext
+{
+    public static SqlConnection CreateConnection()
+    {
+        return new SqlConnection("Data Source=.;Initial Catalog=FileStorage;Integrated Security=true;");
+    }
+}
