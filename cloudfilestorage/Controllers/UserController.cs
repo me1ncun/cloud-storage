710a08363e449d089e3cab58d687fa01ce46fdf5
Denis <denis09112004@gmail.com>
2024-04-22 20:46:12 +0300

    init

---
+++ cloudfilestorage/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cloudfilestorage.Controllers;
+
+public class UserController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
---
+++ cloudfilestorage/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace cloudfilestorage.Models;
+
+public class User
+{
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
