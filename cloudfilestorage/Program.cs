477bc3571bd1ad8218687120186dfc90dbf9e880
Denis <denis09112004@gmail.com>
2024-04-23 21:17:42 +0300

    add di and redis cache

--- cloudfilestorage/Program.cs
+++ cloudfilestorage/Program.cs
@@ -1,7 +1,29 @@
+using cloudfilestorage.Database;
+using cloudfilestorage.Repositories;
+using cloudfilestorage.Repositories.Interface;
+using cloudfilestorage.Services.Implementation;
+using cloudfilestorage.Services.Interface;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IAuthService, AuthService>();
+builder.Services.AddSingleton<IAuthRepository, AuthRepository>();
+builder.Services.AddTransient<AppDbContext>();
+
+// Add Redis cache
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+   options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = "FileStorage.Session";
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+});
 
 var app = builder.Build();
 
@@ -17,6 +39,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
