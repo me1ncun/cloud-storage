166bf3c767d75d37eac682548ab6b002ee8c9e00
Denis <denis09112004@gmail.com>
2024-04-23 21:16:52 +0300

    template

---
+++ cloudfilestorage/Controllers/FileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cloudfilestorage.Controllers;
+
+public class FileController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Upload()
+    {
+        return View();
+    }
+    
+    public IActionResult Download()
+    {
+        return View();
+    }
+    
+}
