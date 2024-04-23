6629680c1f56ba14ff1014252fbecabdce552ce8
Denis <denis09112004@gmail.com>
2024-04-23 21:19:40 +0300

    add required to model

---
+++ cloudfilestorage/Repositories/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cloudfilestorage.Models;
+
+public class User
+{
+    [Required]
+    public string Login { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
