2b1136002f9117a09ab9069d4b4b46f35ea05731
Denis <denis09112004@gmail.com>
2024-05-05 14:56:43 +0300

    set enters

--- cloudfilestorage/Services/Implementation/AuthService.cs
+++ cloudfilestorage/Services/Implementation/AuthService.cs
@@ -4,15 +4,15 @@ using cloudfilestorage.Services.Interface;
 
 namespace cloudfilestorage.Services.Implementation;
 
-public class AuthService: IAuthService
+public class AuthService : IAuthService
 {
     private readonly IAuthRepository _authRepository;
-    
+
     public AuthService(IAuthRepository authRepository)
     {
         _authRepository = authRepository;
     }
-    
+
     public void Register(string login, string password)
     {
         _authRepository.Register(login, password);
@@ -22,7 +22,7 @@ public class AuthService: IAuthService
     {
         return _authRepository.Login(login, password);
     }
-    
+
     public string? Authenticate(string login, string password)
     {
         foreach (User user in _authRepository.FindByLoginAndPass(login, password))
@@ -30,11 +30,13 @@ public class AuthService: IAuthService
             if (user.Password == password)
             {
                 return user.Login;
+                
             }
         }
+
         return null;
     }
-    
+
     public User GetUser(string login, string password)
     {
         return _authRepository.FindByLoginAndPass(login, password).FirstOrDefault();
