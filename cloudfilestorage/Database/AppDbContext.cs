d58536d9fe35a1b82f613483a3fae82f7739e1c5
Denis <denis09112004@gmail.com>
2024-05-05 14:54:39 +0300

    add some enters

--- cloudfilestorage/Database/AppDbContext.cs
+++ cloudfilestorage/Database/AppDbContext.cs
@@ -7,9 +7,12 @@ public class AppDbContext
 {
     private readonly IConfiguration _configuration;
     private readonly string connectionstring;
-    public AppDbContext(IConfiguration configuration) {
+
+    public AppDbContext(IConfiguration configuration)
+    {
         this._configuration = configuration;
         this.connectionstring = this._configuration.GetConnectionString("DefaultConnection");
     }
+
     public IDbConnection CreateConnection() => new SqlConnection(connectionstring);
 }
