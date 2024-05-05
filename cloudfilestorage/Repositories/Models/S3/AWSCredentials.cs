657f6a7e2330e3eece5137938d0c5fe5c37859c3
Denis <denis09112004@gmail.com>
2024-05-05 14:57:08 +0300

    model that saving acccess and secret keys

---
+++ cloudfilestorage/Repositories/Models/S3/AWSCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace cloudfilestorage.Models;
+
+public class AWSCredentials
+{
+    public const string Position = "AWSConfiguration";
+    public string AWSAccessKey { get; set; } = String.Empty;
+    public string AWSSecretKey { get; set; } = String.Empty;
+    
+}
