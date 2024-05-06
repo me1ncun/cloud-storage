1bbe6321ca475e4859a8829a30561defeae7fad7
Denis <denis09112004@gmail.com>
2024-05-06 21:33:33 +0300

    interface of the filestorageservice

---
+++ cloudfilestorage/Services/Interface/IFileStorageService.cs
@@ -0,0 +1,17 @@
+﻿using Amazon.S3.Model;
+using cloudfilestorage.Models;
+
+namespace cloudfilestorage.Services.Interface;
+
+public interface IFileStorageService
+{
+    public Task<bool> UploadFiles(IFormFile file);
+    public Task<bool> CreateBucket(string bucketName);
+    public Task<ListObjectsV2Response> GetAllFiles(string bucketName, string folderName);
+    public Task<bool> DeleteObject(string fileName);
+    public Task<bool> RenameObject(string oldName, string newName);
+    public Task<bool> RenameBucket(string oldName, string newName);
+    public Task<bool> DownloadObject(string fileName);
+    public Task CreateUsersBucket(int userId);
+
+}
