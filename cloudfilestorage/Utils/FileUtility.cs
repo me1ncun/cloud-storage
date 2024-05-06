caf111f8dd7eaf6175efee82e9d754af7fbac247
Denis <denis09112004@gmail.com>
2024-05-06 21:32:47 +0300

    utility that help to take only needed folders and files

---
+++ cloudfilestorage/Utils/FileUtility.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.S3.Model;
+
+namespace cloudfilestorage.Utils;
+
+public static class FileUtility
+{
+    public static ListObjectsV2Response SortFiles(ListObjectsV2Response files, string folderName)
+    {
+        var result = new ListObjectsV2Response();
+        int prefixLength = folderName.Length;
+        foreach (var obj in files.S3Objects)
+        {
+            if (obj.Key.Substring(prefixLength).Trim('/').Split('/').Length == 1  && obj.Key != folderName)
+            {
+                result.S3Objects.Add(obj);
+            }
+        }            
+        return result;
+    }
+}
