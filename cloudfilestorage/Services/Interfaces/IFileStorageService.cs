ebd28d7fd59fa5e5f660d7c49a3a857aade0f870
Denis <denis09112004@gmail.com>
2024-05-09 20:10:50 +0300

    add parameters that i need to work with

--- cloudfilestorage/Services/Interfaces/IFileStorageService.cs
+++ cloudfilestorage/Services/Interfaces/IFileStorageService.cs
@@ -5,12 +5,12 @@ namespace cloudfilestorage.Services.Interface;
 
 public interface IFileStorageService
 {
-    public Task<bool> UploadFiles(IFormFile file);
-    public Task<bool> CreateBucket(string bucketName);
+    public Task<bool> UploadFiles(IFormFile file, string path, string usersFolder);
+    public Task<bool> CreateBucket(string bucketName, string path, string usersFolder);
     public Task<ListObjectsV2Response> GetAllFiles(string bucketName, string folderName);
     public Task<bool> DeleteObject(string fileName);
-    public Task<bool> RenameObject(string oldName, string newName);
-    public Task<bool> RenameBucket(string oldName, string newName);
+    public Task<bool> RenameObject(string oldName, string newName, string path, string usersFolder);
+    public Task<bool> RenameBucket(string oldName, string newName, string path, string usersFolder);
     public Task<bool> DownloadObject(string fileName);
     public Task CreateUsersBucket(int userId);
 
