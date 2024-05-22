3dedda7afeb9451db5d8f51d6f4659159ed95424
Denis <denis09112004@gmail.com>
2024-05-22 17:42:31 +0300

    add attributes to create migration (ef core)

--- cloudfilestorage/Repositories/Models/User.cs
+++ cloudfilestorage/Repositories/Models/User.cs
@@ -1,10 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cloudfilestorage.Models;
-
+[Table("users")]
 public class User
 {
+    [Column("id")]
     public int ID { get; set; }
-    [Required] public string Login { get; set; }
-    [Required] public string Password { get; set; }
+    [Column("login")]
+    [Required] 
+    public string Login { get; set; }
+    [Column("password")]
+    [Required] 
+    public string Password { get; set; }
 }
