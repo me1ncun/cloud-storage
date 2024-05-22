0d27ad655f986f85371875ae99765b3a1390c523
Denis <denis09112004@gmail.com>
2024-05-22 17:42:41 +0300

    extension to use migrations

---
+++ cloudfilestorage/Extensions/MigrationExtension.cs
@@ -0,0 +1,21 @@
+﻿using cloudfilestorage.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace tennis.Extensions;
+
+public static class MigrationExtension
+{
+    public static void ApplyMigrations(this IApplicationBuilder app)
+    {
+        using (var scope = app.ApplicationServices.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+
+            var context = services.GetRequiredService<AppDbContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
